c4923ffd76fbe03b707b1c4a6b5392cf2590be5a
warren vosper <straydogsw@gmail.com>
2017-09-26 16:35:06 -0600

    refactors ask.js to simplify it

--- app/models/ask.js
+++ app/models/ask.js
@@ -3,16 +3,13 @@ const lookupScore = require('../config/scores')
 const createAsk = (attributes = {}) => {
   const ask = Object.assign({}, attributes)
 
-  const attrs = () => ask
-  const score = () => lookupScore(ask.status)
-
   return {
     timestamp: ask.timestamp,
     ask: ask.ask,
     askee: ask.askee,
     status: ask.status,
-    attrs,
-    score,
+    attrs: () => ask,
+    score: () => lookupScore(ask.status),
   }
 }
 
